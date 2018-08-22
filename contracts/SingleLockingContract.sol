7ef9f90d1aec2da39451f2d35a3c7bf78f4fb255
Przemek <przemek@ethworks.io>
2018-08-22 17:08:10 +0200

    Implemented notes and suggestions

--- contracts/SingleLockingContract.sol
+++ contracts/SingleLockingContract.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.4.19;
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract SingleLockingContract is Ownable {
-    using SafeMath for uint256;
-
     /* --- EVENTS --- */
 
     event ReleasedTokens(address indexed _beneficiary);
